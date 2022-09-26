966409807e23ab2d8ead21bfb68ebdcf64413f7a
faizan-99 <mfaizy605@gmail.com>
2022-09-26 18:21:42 +0500

    get all order or get single order api done

--- backend/controller/OrderController.js
+++ backend/controller/OrderController.js
@@ -2,7 +2,7 @@ const Order = require("../models/OrderModel");
 const asyncHandler = require("express-async-handler");
 
 // Create Order
-exports.createOrder = asyncHandler(async (req,res,next) =>{
+exports.createOrder = asyncHandler(async (req, res, next) => {
 
     const {
         shippingInfo,
@@ -22,12 +22,43 @@ exports.createOrder = asyncHandler(async (req,res,next) =>{
         taxPrice,
         shippingPrice,
         totalPrice,
-        paidAt:Date.now(),
+        paidAt: Date.now(),
         user: req.user._id,
     });
 
     res.status(201).json({
         success: true,
         order
+    });
+});
+
+//  Get Single order
+exports.getSingleOrder = asyncHandler(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate(
+        "user",
+        "name email"
+    );
+
+    if (!order) {
+        // return next(new ErrorHandler("Order not found with this id",404));
+
+        res.status(404).json({
+            success: false,
+            message: 'Order not found with this id'
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    });
+});
+
+// Get all orders
+exports.getAllOrders = asyncHandler(async (req,res,next) =>{
+    const orders = await Order.find({user: req.user._id});
+    res.status(200).json({
+        success: true,
+        orders
     });
 });
--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createOrder } = require('../controller/OrderController');
+const { createOrder,getSingleOrder,getAllOrders } = require('../controller/OrderController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 const router = express.Router();
 
 router.route("/createorder").post(isAuthenticatedUser, createOrder);
+router.route("/getorder/:id").get(isAuthenticatedUser, getSingleOrder);
+router.route("/getallorder").get(isAuthenticatedUser, getAllOrders);
 
 module.exports = router
